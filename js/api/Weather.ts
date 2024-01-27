079d051479a1588aa11649b06e7c2c3755c29736
Kiet Ve Tran <kiet.ve.tran@eika.no>
2024-01-27 08:50:06 +0100

    It's correct typescript error by compiling of API fetchWeather.

--- js/api/Weather.ts
+++ js/api/Weather.ts
@@ -1,8 +1,10 @@
+import { QueryFunctionContext } from '@tanstack/react-query';
 import { City } from 'js/domain/CityList';
 import { API_KEY, WeatherResponseData } from 'js/domain/Weather';
 
-export const fetchWeather = async (param: City): Promise<WeatherResponseData> => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${param.lat}&lon=${param.lon}&appid=${API_KEY}`;
+export const fetchWeather = async ({queryKey} : QueryFunctionContext<[string, City]> ): Promise<WeatherResponseData> => {
+    const [_key, city] = queryKey;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${API_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
     return data as WeatherResponseData;
--- js/hook/useFetchWeather.ts
+++ js/hook/useFetchWeather.ts
@@ -1,13 +1,12 @@
-import { QueryObserverOptions, useQuery } from '@tanstack/react-query';
-import * as weatherApi from 'js/api/Weather';
+import { useQuery } from '@tanstack/react-query';
+import { fetchWeather } from 'js/api/Weather';
 import { City } from 'js/domain/CityList';
 import { WeatherResponseData } from 'js/domain/Weather';
 
-const useFetchWeather = (param: City, options?: QueryObserverOptions) => {
-    return useQuery<WeatherResponseData>({
+const useFetchWeather = (param: City) => {
+    return useQuery({
         queryKey: ['weather', param],
-        queryFn: async () => await weatherApi.fetchWeather(param),
-        ...(options ?? {}),
+        queryFn: fetchWeather,
     });
 };
 
